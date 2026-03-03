9d9848b5c4c127ba90a43c6e73744de485bff40f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-03-03 12:56:20 +0000

    Add config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+"""
+Module config
+"""
+import os
+
+
+class Config:
+    """
+    Class Config
+
+    For project settings
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
+        self.pathway = os.path.join(self.warehouse, 'continuous')
+        self.points_ = os.path.join(self.pathway, 'points')
+        self.menu_ = os.path.join(self.pathway, 'menu')
+
+        # Template
+        self.s3_parameters_key = 's3_parameters.yaml'
+        self.arguments_key = 'continuous/arguments.json'
+        self.metadata_ = 'continuous/external'
+
+        # The prefix of the Amazon repository where the quantiles will be stored
+        self.prefix = 'warehouse/continuous'
